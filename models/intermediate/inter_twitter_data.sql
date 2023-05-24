fd3adfc9c26a95b83c278204a38127ff57b58604
Vatsal Patel <vatsalmpatel@gmail.com>
2023-05-24 21:53:44 +0000

    updated inter file for twitter data

---
+++ models/intermediate/inter_twitter_data.sql
@@ -0,0 +1,31 @@
+with int_twitter_data as (
+    select 
+    '' as ad_id,
+    0 as add_to_cart,
+    '' as adset_id,
+    campaign_id,
+    channel,
+    clicks,
+    comments,
+    '' as creative_id,
+    date,
+    engagements,
+    impressions,
+    0 as installs,
+    likes,
+    url_clicks as link_clicks,
+    '' as placement_id,
+    0 as post_click_conversions,
+    0 as post_view_conversions,
+    0 as posts,
+    0 as purchase,
+    0 as registrations,
+    0 as revenue,
+    retweets as shares,
+    spend,
+    0 as total_conversions,
+    total_views as video_views
+
+    from {{ ref('stg_twitter_data') }}
+)
+select * from int_twitter_data
