f5288aae535e122bbba20ab74ff642d819fa0747
Vatsal Patel <vatsalmpatel@gmail.com>
2023-05-24 20:25:49 +0000

    updated staging file for all twitter data

---
+++ models/staging/stg_twitter_data.sql
@@ -0,0 +1,26 @@
+with src_twitter_all_data as (
+    select * from {{ source("mcdm_data_twitter","src_promoted_tweets_twitter_all_data") }}
+),
+
+stg_twitter_all_data as (
+    select 
+    
+    __insert_date as insert_date,
+    campaign_id,
+    "Twitter" as channel,
+    url,
+    text,
+    clicks,
+    comments,
+    date,
+    engagements,
+    impressions,
+    likes,
+    url_clicks,
+    retweets,
+    spend,
+    video_total_views as total_views
+
+    from src_twitter_all_data
+)
+select * from stg_twitter_all_data
